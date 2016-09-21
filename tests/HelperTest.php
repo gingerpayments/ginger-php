83c7fb45c4ddb9d47c718ccba46ace56143c14ec
Ruslan Dmitrijev <ruslan@dmitrijev.net>
2016-09-21 15:09:28 +0200

    Added unit test for Helper.php file

---
+++ tests/HelperTest.php
@@ -0,0 +1,12 @@
+<?php
+
+final class HelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldGetValidModulus()
+    {
+        $this->assertEquals(bcmod("7044060001970316212900", 150), "50");
+    }
+}
