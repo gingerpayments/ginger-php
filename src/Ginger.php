e2ec58ddc6391288cd37e498ca3bdc1a6787d9a4
Frank van den Brink <frank@pragmatist.nl>
2019-09-19 12:03:50 +0200

    Simplified client creation

--- src/Ginger.php
+++ src/Ginger.php
@@ -2,15 +2,14 @@
 
 namespace GingerPayments\Payment;
 
-use GuzzleHttp\Client as HttpClient;
-use Assert\Assertion as Guard;
+use GingerPayments\Payment\HttpClient\CurlHttpClient;
 
 final class Ginger
 {
     /**
      * The library version.
      */
-    const CLIENT_VERSION = '1.2.6';
+    const CLIENT_VERSION = '2.0.0';
 
     /**
      * The API version.
@@ -18,47 +17,16 @@ final class Ginger
     const API_VERSION = 'v1';
 
     /**
-     * API endpoint
-     */
-    const ENDPOINT = 'https://api.gingerpayments.com/{version}/';
-
-    /**
      * Create a new API client.
      *
-     * @param string $apiKey Your API key.
-     * @return Client
-     */
-    public static function createClient($apiKey)
-    {
-        Guard::uuid(
-            static::apiKeyToUuid($apiKey),
-            'Ginger API key is invalid: '.$apiKey
-        );
-
-        return new Client(
-            new HttpClient(
-                [
-                    'base_url' => [self::ENDPOINT, ['version' => self::API_VERSION]],
-                    'defaults' => [
-                        'headers' => [
-                            'User-Agent' => 'ginger-php/'.self::CLIENT_VERSION,
-                            'X-PHP-Version' => PHP_VERSION
-                        ],
-                        'auth' => [$apiKey, '']
-                    ]
-                ]
-            )
-        );
-    }
-
-    /**
-     * Method restores dashes in Ginger API key in order to validate UUID.
-     *
+     * @param string $endpoint
      * @param string $apiKey
-     * @return string UUID
+     * @return ApiClient
      */
-    public static function apiKeyToUuid($apiKey)
+    public static function createClient($endpoint, $apiKey)
     {
-        return preg_replace('/(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/', '$1-$2-$3-$4-$5', $apiKey);
+        return new ApiClient(
+            new CurlHttpClient($endpoint . '/' . self::API_VERSION, $apiKey)
+        );
     }
 }
--- tests/GingerTest.php
+++ tests/GingerTest.php
@@ -2,57 +2,16 @@
 
 namespace GingerPayments\Payment\Tests;
 
+use GingerPayments\Payment\ApiClient;
 use GingerPayments\Payment\Ginger;
 
 final class GingerTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @test
-     */
-    public function itShouldCreateAClient()
+    public function test_it_creates_a_client()
     {
-        $reflectionClass = new \ReflectionClass('GingerPayments\Payment\Client');
-        $reflectionProperty = $reflectionClass->getProperty('httpClient');
-        $reflectionProperty->setAccessible(true);
-
-        $client = Ginger::createClient('f47ac10b58cc4372a5670e02b2c3d479');
-
-        /** @var \GuzzleHttp\Client $httpClient */
-        $httpClient = $reflectionProperty->getValue($client);
-        $this->assertEquals(
-            str_replace('{version}', Ginger::API_VERSION, Ginger::ENDPOINT),
-            $httpClient->getBaseUrl()
-        );
-        $this->assertEquals(
-            [
-                'User-Agent' =>  'ginger-php/' . Ginger::CLIENT_VERSION,
-                'X-PHP-Version' =>  PHP_VERSION
-            ],
-            $httpClient->getDefaultOption('headers')
-        );
-        $this->assertEquals(
-            ['f47ac10b58cc4372a5670e02b2c3d479', ''],
-            $httpClient->getDefaultOption('auth')
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function itShouldFailWithIncorrectAPIkey()
-    {
-        $this->setExpectedException('Assert\InvalidArgumentException');
-        Ginger::createClient('my-api-key');
-    }
-
-    /**
-     * @test
-     */
-    public function itShouldCreateAValidUuid()
-    {
-        $this->assertEquals(
-            Ginger::apiKeyToUuid('f47ac10b58cc4372a5670e02b2c3d479'),
-            'f47ac10b-58cc-4372-a567-0e02b2c3d479'
+        $this->assertInstanceOf(
+            ApiClient::class,
+            Ginger::createClient('https://www.example.com', 'abc123')
         );
     }
 }
